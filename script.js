6e0ad111b9e407dfa31f6ffd4ef3fb863d10b7e5
husniddinuzinfocom <105488050+husniddinuzinfocom@users.noreply.github.com>
2022-08-06 09:16:42 +0500

    add

--- script.js
+++ script.js
@@ -30,49 +30,52 @@ $(".scrollto a").on("click", function () {
   return false;
 });
 
-window.addEventListener("scroll", activeLink);
-const sections = document.querySelectorAll(".tube-content > section");
-const navLi = document.querySelectorAll(".progress__list li");
-function activeLink() {
-  let current = "";
-  let present = "";
-  sections.forEach((section) => {
-    const sectionTop = section.offsetTop;
+if (window.innerWidth > 1024) {
+  window.addEventListener("scroll", activeLink);
+  const sections = document.querySelectorAll(".tube-content > section");
+  const navLi = document.querySelectorAll(".progress__list li");
+  function activeLink() {
+    let current = "";
+    let present = "";
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
 
-    if (pageYOffset >= sectionTop - 60) {
-      current = section.getAttribute("id");
-      const currentPresent =
-        (section.getClientRects()[0].top * -1) /
-        (section.getClientRects()[0].height / 100);
-      if (100 >= currentPresent) {
-        present = Math.ceil(currentPresent);
-      }
-    } else {
-      navLi.forEach((li) => {
-        if (li.classList.contains("active")) {
-          li.querySelector(".link__hidden").style.width = 0 + "%";
+      if (pageYOffset >= sectionTop - 60) {
+        current = section.getAttribute("id");
+        const currentPresent =
+          (section.getClientRects()[0].top * -1) /
+          (section.getClientRects()[0].height / 100);
+        if (100 >= currentPresent) {
+          present = Math.ceil(currentPresent);
         }
-      });
-    }
-  });
+      } else {
+        navLi.forEach((li) => {
+          if (li.classList.contains("active")) {
+            li.querySelector(".link__hidden").style.width = 0 + "%";
+          }
+        });
+      }
+    });
 
-  navLi.forEach((li) => {
-    li.classList.remove("active");
-    if (li.classList.contains(current)) {
-      li.classList.add("active");
-    }
-    if (li.classList.contains("active")) {
-      li.querySelector(".link__hidden").style.width = present + "%";
-      li.querySelector(".link__hidden > span").style.width =
-        li.querySelector("a > span").offsetWidth + "px";
-      li.style.right = present + "%";
-      if (li.nextElementSibling)
-        li.nextElementSibling.querySelector(".link__hidden").style.width = "0%";
-      if (li.previousElementSibling)
-        li.previousElementSibling.querySelector(".link__hidden").style.width =
-          "100%";
-    }
-  });
+    navLi.forEach((li) => {
+      li.classList.remove("active");
+      if (li.classList.contains(current)) {
+        li.classList.add("active");
+      }
+      if (li.classList.contains("active")) {
+        li.querySelector(".link__hidden").style.width = present + "%";
+        li.querySelector(".link__hidden > span").style.width =
+          li.querySelector("a > span").offsetWidth + "px";
+        li.style.right = present + "%";
+        if (li.nextElementSibling)
+          li.nextElementSibling.querySelector(".link__hidden").style.width =
+            "0%";
+        if (li.previousElementSibling)
+          li.previousElementSibling.querySelector(".link__hidden").style.width =
+            "100%";
+      }
+    });
+  }
 }
 
 // if(window.screenX)
@@ -139,38 +142,44 @@ if (window.innerWidth < 1025) {
   const prev = document.querySelector(".prev");
   const navDots = document.querySelectorAll(".nav-dots > li");
   let count = 0;
-  for (let i = 0; i < navDots.length; i++) {
-    if (navDots[i].classList.contains("is-active")) count = i;
-    prev.href = navDots[count].querySelector("a").getAttribute("href");
-    next.href = navDots[count].querySelector("a").getAttribute("href");
-  }
 
-  window.addEventListener("scroll", () => {
+  function mainFunctions() {
     for (let i = 0; i < navDots.length; i++) {
       if (navDots[i].classList.contains("is-active")) count = i;
-      prev.href = navDots[count].querySelector("a").getAttribute("href");
-      next.href = navDots[count].querySelector("a").getAttribute("href");
     }
+    if (count > 0) {
+      prev.href = navDots[count - 1].querySelector("a").getAttribute("href");
+    }
+    if (count < navDots.length - 1) {
+      next.href = navDots[count + 1].querySelector("a").getAttribute("href");
+    }
+    if (!navDots[count + 1]) {     
+      next.classList.add("active");
+    } else {
+      next.classList.remove("active");
+    }
+    if (!navDots[count - 1]) {
+      prev.classList.add("active");
+    } else {
+      prev.classList.remove("active");
+    }
+  }
+  mainFunctions();
+  
+  window.addEventListener("scroll", () => {
+    mainFunctions()
   });
   next.addEventListener("click", () => {
-    for (let i = 0; i < navDots.length; i++) {
-      navDots[i].classList.remove("is-active");
-    }
     if (count < navDots.length - 1) {
       count += 1;
       next.href = navDots[count].querySelector("a").getAttribute("href");
     }
-    navDots[count].classList.add("is-active");
   });
 
   prev.addEventListener("click", () => {
-    for (let i = 0; i < navDots.length; i++) {
-      navDots[i].classList.remove("is-active");
-    }
     if (count > 0) {
       count -= 1;
       prev.href = navDots[count].querySelector("a").getAttribute("href");
     }
-    navDots[count].classList.add("is-active");
   });
 }
